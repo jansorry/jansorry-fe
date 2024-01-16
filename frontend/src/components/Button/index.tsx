69e60cb3fe77a47c60c5b92b906c00fd4707a931
Asher Seo <beomxtone@gmail.com>
2024-01-16 15:12:46 +0900

    feat: 공통 컴포넌트 예시를 버튼 컴포넌트로 작성
    Resolves: #S10P12A107-74

---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react';
+import Link from 'next/link';
+
+import * as styles from './index.css';
+
+interface Props {
+  children: ReactNode;
+  link?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+  type?: 'submit' | 'reset' | 'button';
+}
+
+const Button = ({ link, children, disabled, onClick, type }: Props) => {
+  if (link) {
+    return (
+      <Link href={link} className={styles.commonButton}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    // eslint-disable-next-line react/button-has-type
+    <button type={type} disabled={disabled} className={styles.commonButton} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
