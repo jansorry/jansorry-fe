969311a2f97cb919a576195be80009179a52d148
Asher Seo <beomxtone@gmail.com>
2024-01-18 14:28:43 +0900

    feat: 하단바 공통 컴포넌트로 구현

--- frontend/src/containers/welcome/index.tsx
+++ frontend/src/containers/welcome/index.tsx
@@ -1,9 +1,12 @@
 import WelcomeContent from '@/containers/welcome/Content';
 
+import NavBar from '@/components/NavBar';
+
 const Welcome = () => {
   return (
     <main>
       <WelcomeContent />
+      <NavBar clickedIndex={2} />
     </main>
   );
 };
