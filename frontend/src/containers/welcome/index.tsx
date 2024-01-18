850fc204b88186c8dcfe6e3267f2eccc3ad164af
Asher Seo <beomxtone@gmail.com>
2024-01-18 17:30:50 +0900

    feat: 헤더 공통 컴포넌트 레이아웃 구현
    Related to: #S10P12A107-54

--- frontend/src/containers/welcome/index.tsx
+++ frontend/src/containers/welcome/index.tsx
@@ -1,10 +1,12 @@
 import WelcomeContent from '@/containers/welcome/Content';
 
 import NavBar from '@/components/NavBar';
+import Header from '@/components/Header';
 
 const Welcome = () => {
   return (
     <main>
+      <Header title='헤더 테스트' hasPrevious />
       <WelcomeContent />
       <NavBar clickedIndex={2} />
     </main>
