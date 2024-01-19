c7d4dfc5a610e1c1095cfc44b4383245dbd8e242
Asher Seo <beomxtone@gmail.com>
2024-01-19 15:58:17 +0900

    feat: 랜딩 페이지 반응형으로 작업
    Resolves: #S10P12A107-41

--- frontend/public/svgs/index.ts
+++ frontend/public/svgs/index.ts
@@ -6,5 +6,15 @@ import IconHomeFilled from './home-filled.svg';
 import IconHeart from './heart.svg';
 import IconHeartFilled from './heart-filled.svg';
 import IconArrowLeft from './arrow-left.svg';
+import ButtonKakaoLogin from './kakao-login.svg';
 
-export { IconUser, IconUserFilled, IconHome, IconHomeFilled, IconHeart, IconHeartFilled, IconArrowLeft };
+export {
+  IconUser,
+  IconUserFilled,
+  IconHome,
+  IconHomeFilled,
+  IconHeart,
+  IconHeartFilled,
+  IconArrowLeft,
+  ButtonKakaoLogin,
+};
---
+++ frontend/src/containers/welcome/index.css.ts
@@ -0,0 +1,64 @@
+import { style } from '@vanilla-extract/css';
+
+import { vars } from '@/styles/vars.css';
+
+export const welcomeWrapper = style({
+  height: '100svh',
+  maxWidth: '480px',
+  backgroundColor: vars.colors.oatmeal,
+  textAlign: 'center',
+});
+
+export const bgTop = style({
+  '@media': {
+    'screen and (max-height: 720px)': {
+      marginBottom: 0,
+    },
+    'screen and (min-height: 720px) and (max-height: 840px)': {
+      marginBottom: '32px',
+    },
+  },
+  width: '100%',
+  height: 'auto',
+  marginTop: '16px',
+  marginBottom: '64px',
+});
+
+export const bgBottom = style({
+  width: '100%',
+  height: 'auto',
+  position: 'absolute',
+  bottom: '16px',
+  left: 0,
+  zIndex: 1,
+});
+
+export const title = style({
+  width: '80%',
+  height: 'auto',
+  '@media': {
+    'screen and (max-height: 720px)': {
+      marginBottom: 0,
+    },
+  },
+  marginBottom: '48px',
+  position: 'relative',
+  zIndex: 20,
+});
+
+export const message = style({
+  fontSize: '16px',
+  fontWeight: 'bold',
+  color: vars.colors.brown,
+  position: 'relative',
+  zIndex: 20,
+});
+
+export const kakaoLoginButton = style({
+  width: '80%',
+  height: 'auto',
+  marginTop: '60px',
+  position: 'relative',
+  cursor: 'pointer',
+  zIndex: 20,
+});
