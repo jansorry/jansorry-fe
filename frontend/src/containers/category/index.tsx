86cca988abd01d4eebfb1088cfa0b8b0e8695fa7
Asher Seo <beomxtone@gmail.com>
2024-01-24 01:25:29 +0900

    fix: 잔소리 카테고리 props 타입 수정 및 api 호출 제네릭 추가
    Related to: #S10P12A107-129

--- frontend/src/containers/category/index.tsx
+++ frontend/src/containers/category/index.tsx
@@ -1,25 +1,21 @@
 import { nag } from '@/types/nag';
 
 interface Props {
-  nags: nag[] | null;
+  nags: nag[];
 }
 
 const Category = ({ nags }: Props) => {
   return (
-    <div>
-      {nags ? (
-        nags.map((nagging: nag) => (
-          <>
-            <div>{nagging.nagId}</div>
-            <div>{nagging.categoryId}</div>
-            <div>{nagging.content}</div>
-            <div>{nagging.price}</div>
-          </>
-        ))
-      ) : (
-        <div>서버 에러</div>
-      )}
-    </div>
+    <>
+      {nags.map((nagging: nag) => (
+        <>
+          <div>{nagging.nagId}</div>
+          <div>{nagging.categoryId}</div>
+          <div>{nagging.content}</div>
+          <div>{nagging.price}</div>
+        </>
+      ))}
+    </>
   );
 };
 
--- frontend/src/services/nag.ts
+++ frontend/src/services/nag.ts
@@ -1,10 +1,9 @@
 import { api } from '@/services/index';
 import { nag } from '@/types/nag';
 
-export const getNagCategory = async (): Promise<nag[] | null> => {
+export const getNagCategory = async (): Promise<nag[]> => {
   try {
-    const response: nag[] = await api.get('/nags');
-    return response;
+    return await api.get<nag[]>('/nags');
   } catch (e) {
     console.log(e);
   }
