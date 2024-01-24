4fda9b39061186a570f4bbce1ee24c317d9a7da0
Asher Seo <beomxtone@gmail.com>
2024-01-24 17:06:23 +0900

    build: use standalone

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'standalone',
   webpack: (config, { isServer }) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
     config.resolve.alias['#'] = path.join(__dirname, 'public');
