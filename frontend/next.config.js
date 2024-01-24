da3af6635a6725db4bfb96b1e26d9bb16982bdf1
Asher Seo <beomxtone@gmail.com>
2024-01-24 12:40:53 +0900

    build: webpack 절대경로 추가

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,14 @@
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   output: 'standalone',
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.resolve.alias['#'] = path.join(__dirname, 'public');
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
