64d44e833def86b2a61ff7857e6ac63f752d1a42
seunggwang <sg.ryu23@gmail.com>
2024-01-24 11:04:23 +0900

    commit 수정사항 반영 완료

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -31,7 +31,7 @@ const RootLayout = ({ children }: Props) => {
         </Recoil>
         <Script
           src='https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js'
-          integrity='sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8'
+          integrity={`${process.env.NEXT_PUBLIC_KAKAO_INTEGRITY}`}
           crossOrigin='anonymous'
         />
       </body>
--- frontend/src/containers/welcome/KakaoLoginButton/index.tsx
+++ frontend/src/containers/welcome/KakaoLoginButton/index.tsx
@@ -6,21 +6,19 @@ import Image from 'next/image';
 import * as styles from './index.css';
 
 const redirectUri = process.env.NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI;
-const scope = ['profile_nickname'].join(','); // 유저 닉네임을 받아서 전달
 
 const KakaoLoginButton = () => {
   useEffect(() => {
     const kakaoSDK = document.createElement('script');
     kakaoSDK.async = false;
     kakaoSDK.src = `https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js`;
-    kakaoSDK.integrity = 'sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8';
+    kakaoSDK.integrity = 'process.env.NEXT_PUBLIC_KAKAO_INTEGRITY_KEY';
     kakaoSDK.crossOrigin = 'anonymous';
     document.head.appendChild(kakaoSDK);
 
     const onLoadKakaoAPI = () => {
       if (!window.Kakao.isInitialized()) {
         window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
-        console.log('after Init: ', window.Kakao.isInitialized());
       }
     };
 
@@ -28,12 +26,9 @@ const KakaoLoginButton = () => {
   }, []);
 
   const kakaoLoginHandler = () => {
-    // 인가 코드를 받기 위해서 리다이렉트 페이지로 이동
     window.Kakao.Auth.authorize({
       redirectUri,
-      scope,
     });
-    console.log('Kakao Index Success'); // 카카오 로그인 성공 확인 로그
   };
 
   return (
