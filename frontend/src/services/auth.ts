595b8be644f0cba897b88fedcee00b63e77c2ce1
seunggwang <sg.ryu23@gmail.com>
2024-01-25 10:24:33 +0900

    fe-43 auth.ts code string 타입 수정

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,11 +1,12 @@
 import { api } from '@/services/index';
 import { authResponse } from '@/types/auth';
 
-export const getKakaoLogin = async (code: string | string[]): Promise<authResponse> => {
+export const getKakaoLogin = async (code: string): Promise<authResponse> => {
   try {
     return await api.get<authResponse>(`/oauth/login/kakao?code=${code}`);
   } catch (e) {
     console.log(e);
   }
+  // TODO: authResponse 타입 default 선언 필요
   return { oauthId: '', nickname: null, accessToken: null, refreshToken: null } as authResponse;
 };
