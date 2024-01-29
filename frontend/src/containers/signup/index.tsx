3012261b4329bc6063e7f0014e48899621ae2527
damdam6 <dambi626626@gmail.com>
2024-01-29 10:29:09 +0900

    refactor: signup페이지  contentWrapper 적용
    signup페이지 defaultWrapper와 contentWrapper적용

--- frontend/src/containers/signup/index.tsx
+++ frontend/src/containers/signup/index.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 import Birthyear from '@/containers/signup/Birthyear';
 import Gender from '@/containers/signup/Gender';
 import { userBirthState } from '@/states/signup';
+import { defaultWrapper } from '@/styles/common.css';
+import { contentWrapper } from '@/styles/wrapper.css';
 
 import * as styles from './index.css';
 import Header from '@/components/Header';
@@ -15,7 +16,7 @@ const Signup = () => {
   const [userBirth, setUserBirth] = useRecoilState(userBirthState);
 
   return (
-    <main className={styles.signupWrapper}>
+    <main className={defaultWrapper({ width: 'max', height: 'max' })}>
       <Image
         src='/images/signup/bg.png'
         width={0}
@@ -25,7 +26,7 @@ const Signup = () => {
         className={`${styles.signupBg}`}
       />
       <Header title='회원 가입' />
-      <div className={styles.signupContentWrapper}>{!userBirth ? <Birthyear /> : <Gender />}</div>
+      <div className={contentWrapper({ contentArea: 'headerOnly' })}>{!userBirth ? <Birthyear /> : <Gender />}</div>
     </main>
   );
 };
