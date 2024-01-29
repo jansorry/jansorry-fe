901c8c52865c38bfd2ffab0e6d9a16e541114878
ozllzlme <jin_in_1992@naver.com>
2024-01-29 17:00:20 +0900

    refactor: 스타일 적용 및 애니메이션 기능 추가
    - 스타일 적용 및 애니메이션 기능 추가하였습니다.
    
    Related to: #S10P12A107-216

--- frontend/src/containers/loading/index.css.ts
+++ frontend/src/containers/loading/index.css.ts
@@ -6,6 +6,21 @@ import { defaultWrapper } from '@/styles/common.css';
 export const loadingWrapper = style([
   defaultWrapper({ height: 'max' }),
   {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     textAlign: 'center',
   },
 ]);
+
+export const title = style({
+  '@media': {
+    'screen and (max-height: 600px)': {
+      fontSize: 0,
+    },
+  },
+  fontSize: vars.fontSize['3x'],
+  fontWeight: vars.fontWeight.accent,
+  color: vars.colors.brown,
+});
--- frontend/src/containers/loading/index.tsx
+++ frontend/src/containers/loading/index.tsx
@@ -1,10 +1,14 @@
+import AnimatedNumber from '@/containers/loading/AnimatedNumber';
+import AnimatedImage from '@/containers/loading/AnimatedImage';
+
 import * as styles from './index.css';
 
 const Loading = () => {
   return (
     <main className={styles.loadingWrapper}>
-      <div>당신의 영수증이 발급되고 있습니다.</div>
-      <div>0%</div>
+      <div className={styles.title}>당신의 영수증이 발급되고 있습니다.</div>
+      <AnimatedNumber />
+      <AnimatedImage />
     </main>
   );
 };
