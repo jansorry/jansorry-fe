3574a15740f4ed8ab8bfbb09080309db99474830
Asher Seo <beomxtone@gmail.com>
2024-01-31 17:10:54 +0900

    feat: 잔소리 카드 반응 등록하기 버튼 모달 레이아웃
    re #S10P12A107-254

--- frontend/src/app/createcard/[nagId]/page.tsx
+++ frontend/src/app/createcard/[nagId]/page.tsx
@@ -11,7 +11,13 @@ const CreateCardPage = async ({ params }: Props) => {
   const { nagId } = params;
   const cookieStore = cookies();
   const refreshToken = cookieStore.get('refreshToken')?.value;
-  const data = await getNagDetails(nagId, refreshToken);
+  // const data = await getNagDetails(nagId, refreshToken);
+  const data = {
+    nagId: 5,
+    categoryId: 1,
+    content: '이번 모의고사 몇 등급이니?',
+    price: 30000,
+  };
 
   return <CreateCard {...data} />;
 };
