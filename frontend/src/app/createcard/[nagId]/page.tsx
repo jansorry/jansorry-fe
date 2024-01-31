4168fe49c6604c0250c0f29d89ce5a04882a38ba
Asher Seo <beomxtone@gmail.com>
2024-01-31 17:12:02 +0900

    chore: 더미 데이터 제거

--- frontend/src/app/createcard/[nagId]/page.tsx
+++ frontend/src/app/createcard/[nagId]/page.tsx
@@ -11,13 +11,7 @@ const CreateCardPage = async ({ params }: Props) => {
   const { nagId } = params;
   const cookieStore = cookies();
   const refreshToken = cookieStore.get('refreshToken')?.value;
-  // const data = await getNagDetails(nagId, refreshToken);
-  const data = {
-    nagId: 5,
-    categoryId: 1,
-    content: '이번 모의고사 몇 등급이니?',
-    price: 30000,
-  };
+  const data = await getNagDetails(nagId, refreshToken);
 
   return <CreateCard {...data} />;
 };
