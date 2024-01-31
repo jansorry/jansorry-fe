832762ab715a16c88d97dd401205f86d92fddd4d
Asher Seo <beomxtone@gmail.com>
2024-01-31 13:30:05 +0900

    feat: 잔소리 카드 상세 조회 api
    re #S10P12A107-218

--- frontend/src/types/nag.d.ts
+++ frontend/src/types/nag.d.ts
@@ -2,9 +2,14 @@ export interface nag {
   nagId: number;
   content: string;
   price: number;
+  categoryId?: number;
 }
 
-export interface nagResponse {
+export interface nagDetails extends nag {
+  categoryId: number;
+}
+
+export interface nagTotalResponse {
   categoryId: number;
   title: string;
   nags: nag[];
