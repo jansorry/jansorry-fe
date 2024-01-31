f485be096d208226e145ccc08b95dae89251dd4a
ozllzlme <jin_in_1992@naver.com>
2024-01-31 17:57:10 +0900

    feat: 탈퇴 페이지 생성
    Related to: #S10P12A107-225

---
+++ frontend/src/containers/withdrawal/index.css.tsx
@@ -0,0 +1,34 @@
+import { style } from '@vanilla-extract/css';
+
+import { vars } from '@/styles/vars.css';
+import { contentWrapper } from '@/styles/wrapper.css';
+import { flex } from '@/styles/common.css';
+
+export const withdrawalWrapper = style([
+  flex({ justify: 'spaceAround', align: 'center', direction: 'column' }),
+  {
+    position: 'relative',
+    top: 0,
+    boxSizing: 'border-box',
+    height: '100%',
+    paddingTop: '10%',
+    paddingBottom: '10%',
+  },
+]);
+
+export const withdrawalTitle = style([
+  {
+    padding: vars.space['4x'],
+    fontSize: vars.fontSize['3x'],
+    fontWeight: vars.fontWeight.accent,
+    textAlign: 'center',
+  },
+]);
+
+export const withdrawalContent = style([
+  {
+    padding: vars.space['1x'],
+    fontSize: vars.fontSize['2x'],
+    alignSelf: 'flex-start',
+  },
+]);
