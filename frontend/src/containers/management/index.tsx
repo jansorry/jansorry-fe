dd01567a865a635ccfabc8d76938668b15079491
ozllzlme <jin_in_1992@naver.com>
2024-01-31 15:50:02 +0900

    fix: 잘못된 이름 수정
    - mypage에서 management로 변경
    
    Related to: #S10P12A107-225

---
+++ frontend/src/containers/management/index.tsx
@@ -0,0 +1,80 @@
+import Link from 'next/link';
+
+import { IconEdit, IconInstagram, IconRightBracket, IconTwitter } from '#/svgs';
+
+import * as styles from './index.css';
+import Header from '@/components/Header';
+import NavBar from '@/components/NavBar';
+import { UserPreview } from '@/components/UserPreview';
+
+const managementPage = () => {
+  return (
+    <>
+      <Header title='마이페이지' hasPrevious />
+      <div className={styles.managementWrapper}>
+        <div className={styles.managementProfile}>
+          <UserPreview
+            imgSrc='/images/userProfileImage/temp-userProfile.png'
+            nickname='닉네임'
+            size='xLarge'
+          />
+          <Link href='./change'>
+            <IconEdit />
+          </Link>
+        </div>
+        <hr className={styles.managementSeperateLine} />
+        <Link
+          href='https://ninth-check-14d.notion.site/jansorry-ceb36c9a19864488b242e8d773094950?pvs=4'
+          className={styles.managementListContent}
+        >
+          <div>팀소개</div>
+          <IconRightBracket />
+        </Link>
+        <hr className={styles.managementSeperateLine} />
+        <Link
+          href='https://ninth-check-14d.notion.site/jansorry-ceb36c9a19864488b242e8d773094950?pvs=4'
+          className={styles.managementListContent}
+        >
+          <div>공지사항</div>
+          <IconRightBracket />
+        </Link>
+        <hr className={styles.managementSeperateLine} />
+        <Link
+          href='https://ninth-check-14d.notion.site/jansorry-ceb36c9a19864488b242e8d773094950?pvs=4'
+          className={styles.managementListContent}
+        >
+          <div>자주 듣는 질문</div>
+          <IconRightBracket />
+        </Link>
+        <hr className={styles.managementSeperateLine} />
+        <Link href='/logout' className={styles.managementListContent}>
+          <div>로그아웃</div>
+          <IconRightBracket />
+        </Link>
+        <hr className={styles.managementSeperateLine} />
+        <Link href='/withdrawl' className={styles.managementListContent}>
+          <div>회원 탈퇴</div>
+          <IconRightBracket />
+        </Link>
+        <hr className={styles.managementSeperateLine} />
+        <Link
+          href='https://www.instagram.com/jansorry_official/'
+          className={styles.managementSnsListContent}
+        >
+          <IconInstagram />
+          <div>인스타그램</div>
+        </Link>
+        <Link
+          href='https://twitter.com/jansorry_S2'
+          className={styles.managementSnsListContent}
+        >
+          <IconTwitter />
+          <div>트위터</div>
+        </Link>
+      </div>
+      <NavBar clickedIndex={3} />
+    </>
+  );
+};
+
+export default managementPage;
