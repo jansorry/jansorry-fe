75e54e89b0b227719895adc36e712dc66553e0c2
ozllzlme <jin_in_1992@naver.com>
2024-02-01 14:59:22 +0900

    feat: 닉네임 수정 레이아웃 완료
    Related to: #S10P12A107-237

---
+++ frontend/src/containers/nicknameEdit/index.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { ChangeEvent, useState } from 'react';
+
+import { contentWrapper } from '@/styles/wrapper.css';
+import { MAX_ACTION_INPUT_LENGTH } from '@/constants';
+
+import Header from '@/components/Header';
+import NavBar from '@/components/NavBar';
+import Button from '@/components/Button';
+import * as styles from './index.css';
+import Modal from '@/components/Modal';
+
+const nicknameEdit = () => {
+  const [inputCount, setInputCount] = useState<number>(0);
+
+  return (
+    <>
+      <Header title='닉네임 변경하기' hasPrevious />
+      <div
+        className={contentWrapper({
+          contentArea: 'headerAndNavBar',
+          heightStyle: 'max',
+        })}
+      >
+        <div className={styles.nicknameEditWrapper}>
+          <div>
+            <div className={styles.nicknameEditTitle}>
+              변경할 닉네임을 입력해주세요
+            </div>
+            <div className={styles.nicknameInputWrapper}>
+              <input
+                type='text'
+                className={styles.nicknameInputStyle}
+                maxLength={10}
+              />
+            </div>
+            <div className={styles.nicknameEditContent}>
+              - 닉네임은 최대 10자까지 가능해요.
+            </div>
+            <div className={styles.nicknameEditContent}>
+              - 알파벳 대소문자, 한글, 숫자만 가능해요.
+            </div>
+          </div>
+
+          <Button type='button' size='large' colorStyle='blue' filled>
+            닉네임 변경하기
+          </Button>
+        </div>
+      </div>
+      <NavBar />
+    </>
+  );
+};
+
+export default nicknameEdit;
