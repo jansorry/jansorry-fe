5ab1a57f89678de26699ceaa3a337393c3f351eb
ozllzlme <jin_in_1992@naver.com>
2024-02-02 12:27:47 +0900

    feat: Loading 애니메이션 추가
    로딩 애니메이션 추가
    
    Related to: #S10P12A107-304

--- frontend/src/components/Loading/index.tsx
+++ frontend/src/components/Loading/index.tsx
@@ -1,5 +1,15 @@
+import Image from 'next/image';
+
+import { IconLoading } from '#/svgs';
+
+import * as styles from './index.css';
+
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className={styles.LoadingWrapper}>
+      <IconLoading />
+    </div>
+  );
 };
 
 export default Loading;
