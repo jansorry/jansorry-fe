b6768948d7119ae1a978d8d0517b1f1c398ce9a7
Asher Seo <beomxtone@gmail.com>
2024-02-07 17:08:47 +0900

    chore: remove dummy

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -6,8 +6,7 @@ import { getHomeCards } from '@/services/home';
 const HomePage = async () => {
   const cookieStore = cookies();
   const refreshToken = cookieStore.get('refreshToken')?.value;
-  // const data = await getHomeCards(refreshToken);
-  const data = { count: 3, categoryList: [1, 2, 3] };
+  const data = await getHomeCards(refreshToken);
 
   return <Home {...data} />;
 };
