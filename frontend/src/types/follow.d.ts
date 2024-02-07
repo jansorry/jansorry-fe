bbc2d540f70cbc588675a42fa02a87c603811875
damdam6 <dambi626626@gmail.com>
2024-02-07 12:58:09 +0900

    refactor :불필요 코드 삭제
    코드 삭제

--- frontend/src/types/follow.d.ts
+++ frontend/src/types/follow.d.ts
@@ -4,14 +4,10 @@ interface userInfo {
 }
 export interface followerResponse extends userInfo {
   fromId: number;
-  imageUrl: number;
-  nickname: string;
 }
 
 export interface followingResponse extends userInfo {
   toId: number;
-  imageUrl: number;
-  nickname: string;
 }
 
 export interface searchResponse extends userInfo {
