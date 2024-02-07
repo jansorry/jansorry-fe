1e63bf39656df455b80801be5ec098140b2767ca
seunggwang <sg.ryu23@gmail.com>
2024-02-08 05:01:10 +0900

    fix: mypage followe, following 부분 버튼 transparent 작업
    Related to: #S10P12A107-193

--- frontend/src/containers/mypage/UserProfile.tsx
+++ frontend/src/containers/mypage/UserProfile.tsx
@@ -34,17 +34,25 @@ export const UserProfile = ({
           <span className={styles.nicknameBox}>{nickname}</span>
           <button
             type='button'
-            className={styles.settingsButton}
+            className={styles.transparentButton}
             onClick={() => handleConfigClicked()}
           >
             <IconSetting />
           </button>
         </div>
         <div className={styles.followInfo}>
-          <button type='button' onClick={() => router.push('/followers')}>
+          <button
+            type='button'
+            className={styles.transparentButton}
+            onClick={() => router.push('/followers')}
+          >
             <div className={styles.followNumber}>팔로워 {followerCnt}</div>
           </button>
-          <button type='button' onClick={() => router.push('/followings')}>
+          <button
+            type='button'
+            className={styles.transparentButton}
+            onClick={() => router.push('/followings')}
+          >
             <div className={styles.followNumber}>팔로잉 {followingCnt}</div>
           </button>
         </div>
