2cb56aefb29f6d98eefc5e15d65a5d63c0942041
Asher Seo <beomxtone@gmail.com>
2024-02-15 10:22:38 +0900

    fix: change feed page ssr to csr
    re #S10P12A107-436

--- frontend/src/app/feed/page.tsx
+++ frontend/src/app/feed/page.tsx
@@ -1,14 +1,1 @@
-import { cookies } from 'next/headers';
-
-import { getLiveFeedonServer } from '@/services/feed';
-import Feed from '@/containers/feed';
-
-const FeedPage = async () => {
-  const cookieStore = cookies();
-  const refreshToken = cookieStore.get('refreshToken')?.value;
-  const data = await getLiveFeedonServer(refreshToken);
-
-  return <Feed {...data} />;
-};
-
-export default FeedPage;
+export { default } from '@/containers/feed';
