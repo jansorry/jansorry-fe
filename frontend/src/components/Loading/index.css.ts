2d1f853029f5556c01d3e1bc50a48aeb39244ff7
Asher Seo <beomxtone@gmail.com>
2024-02-15 10:44:48 +0900

    fix: 로딩 zindex 수정

--- frontend/src/components/Loading/index.css.ts
+++ frontend/src/components/Loading/index.css.ts
@@ -1,11 +1,14 @@
 import { style } from '@vanilla-extract/css';
 
 import { defaultWrapper, flex } from '@/styles/common.css';
+import { vars } from '@/styles/vars.css';
 
 export const LoadingWrapper = style([
   flex({ justify: 'center', align: 'center', direction: 'column' }),
   defaultWrapper({ height: 'max' }),
   {
+    position: 'relative',
     textAlign: 'center',
+    zIndex: vars.zIndex['layout-content'],
   },
 ]);
